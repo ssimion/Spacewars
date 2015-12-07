aa2bed2eeaab3d934922a19de65475c2d090decc
unknown <simions2@EC214-ECWS-18.coventry.ac.uk>
2015-12-07 13:35:20 +0000

    sdfsdf

--- Scripts/JS/multiplayerGlobals.js
+++ Scripts/JS/multiplayerGlobals.js
@@ -1,8 +1,1 @@
 
-var currentPlayer = "";
-var opposingPlayer = "";
-var opposingPlayerID = "";
-var matchVersion = 0;
-var matchID = 0;
-var matchDataReceived = false;
-var usersFriends = new Array();
